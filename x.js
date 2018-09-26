29c68c60b776d2f6756b1638d75685c616c39d8c
Charlotte14 <vaudourc@yahoo.fr>
2018-09-26 18:55:00 +0200

    Ajoutx.js

---
+++ x.js
@@ -0,0 +1,5 @@
+setTimeout(function(){alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+if (confirm('Etes-vous sûr?'))
+
+{open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');}
+}, 10000);
